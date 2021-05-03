5172d45620534ffa6af6fbefb1d1637d1bda1720
Aleksey.Shiryaev <Aleksey.Shiryaev@payever.org>
2021-05-03 04:18:39 +0400

    feat: add Planets filter

--- src/components/Planets/PlanetsContainer.jsx
+++ src/components/Planets/PlanetsContainer.jsx
@@ -2,81 +2,9 @@ import React from "react";
 import Planets from "./Planets";
 import moment from "moment";
 import reqwest from "reqwest";
-
-const COLUMNS = [
-  {
-    title: "Name",
-    dataIndex: "name",
-    key: "name",
-    responsive: ["sm"],
-  },
-  {
-    title: "Rotation period",
-    dataIndex: "rotation_period",
-    key: "rotation_period",
-    responsive: ["sm"],
-  },
-  {
-    title: "Orbital period",
-    dataIndex: "orbital_period",
-    key: "orbital_period",
-    responsive: ["sm"],
-  },
-  {
-    title: "Diameter",
-    dataIndex: "diameter",
-    key: "diameter",
-    responsive: ["sm"],
-  },
-  {
-    title: "Climate",
-    dataIndex: "climate",
-    key: "climate",
-    responsive: ["sm"],
-  },
-  {
-    title: "Gravity",
-    dataIndex: "gravity",
-    key: "gravity",
-    responsive: ["sm"],
-  },
-  {
-    title: "Terrain",
-    dataIndex: "terrain",
-    key: "terrain",
-    responsive: ["sm"],
-  },
-  {
-    title: "Surface water",
-    dataIndex: "surface_water",
-    key: "surface_water",
-    responsive: ["sm"],
-  },
-  {
-    title: "Population",
-    dataIndex: "population",
-    key: "population",
-    responsive: ["sm"],
-  },
-  {
-    title: "Created",
-    dataIndex: "created",
-    key: "created",
-    responsive: ["sm"],
-    render: (text) => {
-      return moment(text).format("MMMM Do YYYY, h:mm:ss a");
-    },
-  },
-  {
-    title: "Edited",
-    dataIndex: "edited",
-    key: "edited",
-    responsive: ["sm"],
-    render: (text) => {
-      return moment(text).format("MMMM Do YYYY, h:mm:ss a");
-    },
-  },
-];
+import Highlighter from "react-highlight-words";
+import {SearchOutlined} from "@ant-design/icons";
+import {Button, Input, Space} from "antd";
 
 class PlanetsContainer extends React.Component {
   state = {
@@ -87,6 +15,8 @@ class PlanetsContainer extends React.Component {
     },
     count: Number,
     loading: false,
+    searchText: "",
+    searchedColumn: "",
   };
 
   componentDidMount() {
@@ -143,27 +73,180 @@ class PlanetsContainer extends React.Component {
     });
   };
 
-  // componentDidMount() {
-  //   axios
-  //     .get(
-  //       `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
-  //     )
-  //     .then((response) => {
-  //       this.props.setUsers(response.data.items);
-  //       this.props.setTotalUsersCount(response.data.totalCount);
-  //     });
-  // }
+  getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+    }) => (
+      <div style={{padding: 8}}>
+        <Input
+          ref={(node) => {
+            this.searchInput = node;
+          }}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() =>
+            this.handleSearch(selectedKeys, confirm, dataIndex)
+          }
+          style={{width: 188, marginBottom: 8, display: "block"}}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => this.handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{width: 90}}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => this.handleReset(clearFilters)}
+            size="small"
+            style={{width: 90}}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({closeDropdown: false});
+              this.setState({
+                searchText: selectedKeys[0],
+                searchedColumn: dataIndex,
+              });
+            }}
+          >
+            Filter
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined style={{color: filtered ? "#1890ff" : undefined}} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        ? record[dataIndex]
+            .toString()
+            .toLowerCase()
+            .includes(value.toLowerCase())
+        : "",
+    onFilterDropdownVisibleChange: (visible) => {
+      if (visible) {
+        setTimeout(() => this.searchInput.select(), 100);
+      }
+    },
+    render: (text) =>
+      this.state.searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{backgroundColor: "#ffc069", padding: 0}}
+          searchWords={[this.state.searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ""}
+        />
+      ) : (
+        text
+      ),
+  });
 
-  // handleTableChange = (pagination, filters, sorter) => {
-  //   this.fetch({
-  //     sortField: sorter.field,
-  //     sortOrder: sorter.order,
-  //     pagination,
-  //     ...filters,
-  //   });
-  // };
+  handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    this.setState({
+      searchText: selectedKeys[0],
+      searchedColumn: dataIndex,
+    });
+  };
+
+  handleReset = (clearFilters) => {
+    clearFilters();
+    this.setState({searchText: ""});
+  };
 
   render() {
+    const COLUMNS = [
+      {
+        title: "Name",
+        dataIndex: "name",
+        key: "name",
+        responsive: ["sm"],
+        ...this.getColumnSearchProps("name"),
+      },
+      {
+        title: "Rotation period",
+        dataIndex: "rotation_period",
+        key: "rotation_period",
+        responsive: ["sm"],
+      },
+      {
+        title: "Orbital period",
+        dataIndex: "orbital_period",
+        key: "orbital_period",
+        responsive: ["sm"],
+      },
+      {
+        title: "Diameter",
+        dataIndex: "diameter",
+        key: "diameter",
+        responsive: ["sm"],
+        ...this.getColumnSearchProps("diameter"),
+      },
+      {
+        title: "Climate",
+        dataIndex: "climate",
+        key: "climate",
+        responsive: ["sm"],
+      },
+      {
+        title: "Gravity",
+        dataIndex: "gravity",
+        key: "gravity",
+        responsive: ["sm"],
+      },
+      {
+        title: "Terrain",
+        dataIndex: "terrain",
+        key: "terrain",
+        responsive: ["sm"],
+      },
+      {
+        title: "Surface water",
+        dataIndex: "surface_water",
+        key: "surface_water",
+        responsive: ["sm"],
+      },
+      {
+        title: "Population",
+        dataIndex: "population",
+        key: "population",
+        responsive: ["sm"],
+        ...this.getColumnSearchProps("population"),
+      },
+      {
+        title: "Created",
+        dataIndex: "created",
+        key: "created",
+        responsive: ["sm"],
+        render: (text) => {
+          return moment(text).format("MMMM Do YYYY, h:mm:ss a");
+        },
+      },
+      {
+        title: "Edited",
+        dataIndex: "edited",
+        key: "edited",
+        responsive: ["sm"],
+        render: (text) => {
+          return moment(text).format("MMMM Do YYYY, h:mm:ss a");
+        },
+      },
+    ];
     const {data, pagination, loading} = this.state;
     return (
       <Planets
